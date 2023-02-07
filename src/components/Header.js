afc62f8de976ca236e4af6f249e06d345a37eafa
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-07 19:13:01 +0600

    different sagment done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs'
+import img1 from '../images/compare.svg'
+import img2 from '../images/wishlist.svg'
+import img3 from '../images/user.svg'
+import img4 from '../images/cart.svg'
 const Header = () => {
     return (
         <>
@@ -18,22 +22,51 @@ const Header = () => {
             </header>
             <header className='header-upper py-3'>
                 <div className='container-xxl'>
-                    <div className='row'>
+                    <div className='row align-items-center'>
                         <div className='col-2'>
-                            <h1>
+                            <h2>
                                 <Link className='text-white'>Stella</Link>
-                            </h1>
+
+                            </h2>
                         </div>
                         <div className='col-5'>
-                            <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Search Products Here" aria-label="Search Products Here" aria-describedby="basic-addon2" />
-                                <span className="input-group-text" id="basic-addon2">
-                                    <BsSearch />
+                            <div className="input-group">
+                                <input type="text" className="form-control py-2" placeholder="Search Products Here" aria-label="Search Products Here" aria-describedby="basic-addon2" />
+                                <span className="input-group-text p-3" id="basic-addon2">
+                                    <BsSearch className='fs-6' />
                                 </span>
                             </div>
                         </div>
                         <div className='col-5'>
-
+                            <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={img1} alt="compare" />
+                                        <p className='mb-0'>Compare <br /> products</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={img2} alt="wishList" />
+                                        <p className='mb-0'>Favourite<br /> Wishlist</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={img3} alt="" />
+                                        <p className='mb-0'>Login <br /> My Account</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={img4} alt="" />
+                                        <div className='d-flex flex-column gap-10'>
+                                            <span className='badge bg-white text-dark'>0</span>
+                                            <p className='mb-0'>$500</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
