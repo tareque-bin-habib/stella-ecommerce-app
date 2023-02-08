8ce70c1cd67066ffb4cc6917994523d2aa3d614b
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-08 12:15:24 +0600

    copyright section added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <>
+            <footer className='py-3'></footer>
+            <footer className='py-3'></footer>
+            <footer className='py-4'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <p className='text-center mb-0 text-white'>&copy; {new Date().getFullYear()}: Powered by Stella</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
     );
 };
 
