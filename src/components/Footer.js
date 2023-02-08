9e4780672941a3c45161b35d55f47d8d7fa9d741
tareque-bin-habib <shad.tbh@gmail.com>
2023-02-08 13:29:29 +0600

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img1 from '../images/newsletter.png'
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+import { AiFillYoutube } from 'react-icons/ai';
+import { BsInstagram } from 'react-icons/bs';
+
 
 const Footer = () => {
     return (
@@ -24,7 +30,62 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-            <footer className='py-3'></footer>
+            <footer className='py-4'>
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-4">
+                            <h4 className='text-white mb-4'>Contact Us</h4>
+                            <div>
+                                <address className='text-white fs-6'>Uttora R/A, Agrabad, <br /> Chittagong, Bangladesh</address>
+                                <a className='mt-3 d-block mb-0 text-white' href="tel:+880 4213464314">+880 1452364255</a>
+                                <a className='mt-2 d-block mb-0 text-white' href="shads.tbh@gmail.com">shads.tbh@gmail.com</a>
+                                <div className='socila-icons d-flex align-items-center gap-30 mt-4'>
+                                    <a className='text-white' href="linkedin">
+                                        <AiFillLinkedin className='fs-4' />
+                                    </a>
+                                    <a className='text-white' href="insta">
+                                        <BsInstagram className='fs-4' />
+                                    </a>
+                                    <a className='text-white' href="git">
+                                        <AiFillGithub className='fs-4' />
+                                    </a>
+                                    <a className='text-white' href="you">
+                                        <AiFillYoutube className='fs-4' />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className='text-white mb-4'>Information</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Refund Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Shipping Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Terms and Condition</Link>
+                                <Link className='text-white py-2 mb-1'>Blogs</Link>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className='text-white mb-4'>Account</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>About us</Link>
+                                <Link className='text-white py-2 mb-1'>Faq</Link>
+                                <Link className='text-white py-2 mb-1'>Contact</Link>
+
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <h4 className='text-white mb-4'>Quick links</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>Laptop</Link>
+                                <Link className='text-white py-2 mb-1'>Headphone</Link>
+                                <Link className='text-white py-2 mb-1'>Tablet</Link>
+                                <Link className='text-white py-2 mb-1'>Watch</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
             <footer className='py-4'>
                 <div className='container-xxl'>
                     <div className='row'>
